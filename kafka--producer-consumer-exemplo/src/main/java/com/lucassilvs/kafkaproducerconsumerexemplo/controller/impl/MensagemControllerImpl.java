5266a5d130c2aca3fb09700f11d59e267cadb357
Lucas Santos <lucas.parkl@outlook.com>
2022-12-29 18:01:21 -0300

    feature: implementando Kafka producer: - ajustando timeout application.yaml - adicionando logger ao postar mensagem no arquivo KafkaPostUtils - adicionando anotação para injeção de dependencia nas classes MensagemControllerImpl MensagemServiceImpl

--- kafka--producer-consumer-exemplo/src/main/java/com/lucassilvs/kafkaproducerconsumerexemplo/controller/impl/MensagemControllerImpl.java
+++ kafka--producer-consumer-exemplo/src/main/java/com/lucassilvs/kafkaproducerconsumerexemplo/controller/impl/MensagemControllerImpl.java
@@ -4,6 +4,7 @@ package com.lucassilvs.kafkaproducerconsumerexemplo.controller.impl;
 import com.lucassilvs.kafkaproducerconsumerexemplo.controller.MensagemController;
 import com.lucassilvs.kafkaproducerconsumerexemplo.models.request.MensagemRequest;
 import com.lucassilvs.kafkaproducerconsumerexemplo.service.MensagemService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,7 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/mensagem")
 public class MensagemControllerImpl  implements MensagemController {
 
-
+    @Autowired
     private MensagemService mensagemService;
 
     @PostMapping("/postar-mensagem")
--- kafka--producer-consumer-exemplo/src/main/java/com/lucassilvs/kafkaproducerconsumerexemplo/gateways/KafkaPostUtils.java
+++ kafka--producer-consumer-exemplo/src/main/java/com/lucassilvs/kafkaproducerconsumerexemplo/gateways/KafkaPostUtils.java
@@ -4,9 +4,12 @@ package com.lucassilvs.kafkaproducerconsumerexemplo.gateways;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.logging.Logger;
+
 @Component
 public class KafkaPostUtils {
 
+    private Logger logger = Logger.getLogger(this.getClass().getName());
     private final KafkaTemplate<String,String> kafkaTemplate;
 
     public KafkaPostUtils(KafkaTemplate<String, String> kafkaTemplate) {
@@ -14,6 +17,9 @@ public class KafkaPostUtils {
     }
 
     public void postarMensagem(String mensagem, String nomeTopico){
+        logger.info("Postando mensagem: " + mensagem + " no tópico " + nomeTopico);
         kafkaTemplate.send(nomeTopico, mensagem);
+        logger.info("mensagem enviada com sucesso");
+
     }
 }
