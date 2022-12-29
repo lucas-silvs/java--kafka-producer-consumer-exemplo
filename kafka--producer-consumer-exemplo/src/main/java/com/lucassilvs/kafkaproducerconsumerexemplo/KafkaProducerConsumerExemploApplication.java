cfcc2f3fffdebd8c7fcdf3c9b29839ba59792640
Lucas Santos <lucas.parkl@outlook.com>
2022-12-29 16:46:16 -0300

    envio do projeto inicial

---
+++ kafka--producer-consumer-exemplo/src/main/java/com/lucassilvs/kafkaproducerconsumerexemplo/KafkaProducerConsumerExemploApplication.java
@@ -0,0 +1,13 @@
+package com.lucassilvs.kafkaproducerconsumerexemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducerConsumerExemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaProducerConsumerExemploApplication.class, args);
+	}
+
+}
---
+++ kafka--producer-consumer-exemplo/src/test/java/com/lucassilvs/kafkaproducerconsumerexemplo/KafkaProducerConsumerExemploApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucassilvs.kafkaproducerconsumerexemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaProducerConsumerExemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
