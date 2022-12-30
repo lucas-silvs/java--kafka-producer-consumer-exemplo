4f3f667759b0dd4d1c28a77989454fd0c4c228cf
Lucas Santos <lucas.parkl@outlook.com>
2022-12-30 18:26:32 -0300

    feature: utilizando Schema Registry e Avro: - removendo dependencia inutilizada

--- kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/gateways/KafkaPostUtils.java
+++ kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/gateways/KafkaPostUtils.java
@@ -1,7 +1,6 @@
 package com.lucassilvs.kafkaproducerexemplo.gateways;
 
 
-import com.lucassilvs.kafkaproducerexemplo.gateways.kafka.UsuarioTesteAvro;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
