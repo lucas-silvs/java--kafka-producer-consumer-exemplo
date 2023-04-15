6b3808b5ea0b79661737b1fcfb580394eb6a201e
Lucas Santos <lucas.parkl@outlook.com>
2023-04-15 18:41:18 -0300

    feature: utilização cluster Kafka Confluent Cloud: - ajustando consumer para integração com a Confluent Cloud - ajustando para remoção de propriedades Java ao validar Schema Registry - alteração de mapeação de propriedades do Consumer Kafka para mapeamento manual (properties e variaveis de ambiente) - criação de profile 'confluent' para teste utilizando as properties da Confluent Cloud

--- kafka--consumer-exemplo/src/main/java/com/lucassilvs/kafkaconsumerexemplo/controller/impl/KafkaConsumerListenerAvroImpl.java
+++ kafka--consumer-exemplo/src/main/java/com/lucassilvs/kafkaconsumerexemplo/controller/impl/KafkaConsumerListenerAvroImpl.java
@@ -2,17 +2,21 @@ package com.lucassilvs.kafkaconsumerexemplo.controller.impl;
 
 import com.lucassilvs.kafkaproducerexemplo.gateways.kafka.UsuarioTesteAvro;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Profile("avro")
+@Profile({"avro", "confluent"})
 @Service
 public class KafkaConsumerListenerAvroImpl  {
+
+    private Logger logger = LoggerFactory.getLogger(KafkaConsumerListenerAvroImpl.class);
 
     @KafkaListener(topics = "${spring.kafka.nome-topico}", containerFactory = "kafkalisternerContainerFactory", groupId = "${spring.kafka.consumer.group-id}")
     public void consumindoMensagemAvro(ConsumerRecord<String,UsuarioTesteAvro> mensagemSerialized) {
         UsuarioTesteAvro mensagem = mensagemSerialized.value();
-        System.out.println("Recebendo mensagem Avro - nome: " + mensagem.getNome() + " id: " + mensagem.getId() + " saldo: " + mensagem.getSaldo());
+        logger.info(String.format("Recebendo mensagem Avro - nome: %s id: %s saldo: %s", mensagem.getNome(), mensagem.getId(), mensagem.getSaldo());
     }
 }
