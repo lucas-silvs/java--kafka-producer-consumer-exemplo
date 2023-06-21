d82b778d4099a14fb9002f5c067a90408fd547ea
Lucas Santos <lucas.parkl@outlook.com>
2023-06-21 00:26:18 -0300

    WIP: utilização de annotations de criação dinamica de beans para implementar producers/consumers de forma dinamica, sem a necessidade de instanciar beans:
    - criação da annotacion KafkaProducer
    - criação da classe KafkaListenerCustomConfig para criar os beans dos consumers

---
+++ lib--kafka-clients/src/main/java/com/lucassilvs/libkafkaclients/annotation/consumer/KafkaListenerCustomConfig.java
@@ -0,0 +1,46 @@
+package com.lucassilvs.libkafkaclients.annotation.consumer;
+
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
+import org.springframework.beans.factory.config.ConfigurableListableBeanFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+
+import java.util.Map;
+
+@Configuration
+public class KafkaListenerCustomConfig {
+
+    private Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listConsumers;
+
+    public KafkaListenerCustomConfig(@Qualifier("listConsumers") Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listConsumers) {
+        this.listConsumers = listConsumers;
+    }
+
+
+    @Bean
+    public BeanFactoryPostProcessor dynamicConsumerBeanFactoryPostProcessor() {
+        return new DynamicConsumerBeanFactoryPostProcessor(this.listConsumers);
+    }
+
+    private static class DynamicConsumerBeanFactoryPostProcessor implements BeanFactoryPostProcessor {
+
+        private Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listConsumers;
+
+        public DynamicConsumerBeanFactoryPostProcessor(Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listConsumers) {
+            this.listConsumers = listConsumers;
+        }
+        @Override
+        public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) throws BeansException {
+            for (Map.Entry<String, ConcurrentKafkaListenerContainerFactory<String, Object>> entry : listConsumers.entrySet()) {
+                String beanName = entry.getKey();
+                ConcurrentKafkaListenerContainerFactory<String, Object> containerFactory = entry.getValue();
+                beanFactory.registerSingleton(beanName, containerFactory);
+            }
+
+        }
+    }
+}
---
+++ lib--kafka-clients/src/main/java/com/lucassilvs/libkafkaclients/annotation/producer/KafkaProducerConfig.java
@@ -0,0 +1,69 @@
+package com.lucassilvs.libkafkaclients.annotation.producer;
+
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+
+import java.lang.reflect.Field;
+import java.util.Map;
+
+@Configuration
+public class KafkaProducerConfig {
+
+
+    @Bean
+    public KafkaProducerConfigBeanPostProcessor kafkaClientBeanPostProcessor(@Qualifier("listProducers") Map<String, KafkaTemplate> listaProducers,
+                                                                             @Qualifier("listConsumers") Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listaConsumers) {
+        return new KafkaProducerConfigBeanPostProcessor(listaProducers, listaConsumers);
+    }
+
+    private static class KafkaProducerConfigBeanPostProcessor implements BeanPostProcessor {
+
+        private final Map<String, KafkaTemplate> listaProducers;
+        private final Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listaConsumers;
+
+        public KafkaProducerConfigBeanPostProcessor( Map<String, KafkaTemplate> listaProducers,
+                                                     Map<String, ConcurrentKafkaListenerContainerFactory<String, Object>> listaConsumers) {
+            this.listaProducers = listaProducers;
+            this.listaConsumers = listaConsumers;
+        }
+
+
+        @Override
+        public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+            return bean;
+        }
+
+        @Override
+        public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+            Class<?> beanClass = bean.getClass();
+            Field[] fields = beanClass.getDeclaredFields();
+            for (Field field : fields) {
+                if (field.isAnnotationPresent(KafkaProducer.class)) {
+                    KafkaProducer kafkaClientAnnotation = field.getAnnotation(KafkaProducer.class);
+                    String kafkaClientName = kafkaClientAnnotation.name();
+                    KafkaTemplate kafkaClient = getKafkaProducerByName(kafkaClientName);
+                    field.setAccessible(true);
+                    try {
+                        field.set(bean, kafkaClient);
+                    } catch (IllegalAccessException e) {
+                        throw new BeansException("Error setting Kafka client field: " + field.getName(), e) {
+                        };
+                    }
+                }
+            }
+            return bean;
+        }
+
+        // Implemente a lógica para obter o Kafka Producer/Consumer pelo nome fornecido
+        private KafkaTemplate getKafkaProducerByName(String name) {
+            // Retorne o Kafka Producer/Consumer com base no nome
+            return listaProducers.get(name);
+        }
+    }
+}
+
