a49a19733863c21153c676ad0576a07cf59c630b
Lucas Santos <lucas.parkl@outlook.com>
2023-07-27 18:26:34 -0300

    WIP: utilização de annotations de criação dinamica de beans para implementar producers/consumers de forma dinamica, sem a necessidade de instanciar beans:
    - removendo TODOS

---
+++ kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/config/kafka/ProducerConfigProtobuf.java
@@ -0,0 +1,70 @@
+package com.lucassilvs.kafkaproducerexemplo.config.kafka;
+
+import com.lucassilvs.kafkaproducerexemplo.gateways.kafka.UsuarioTesteAvro;
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializer;
+import io.confluent.kafka.serializers.protobuf.KafkaProtobufSerializerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Profile("protobuf")
+@Configuration
+public class ProducerConfigProtobuf {
+
+    @Value("${spring.kafka.bootstrap-servers}")
+    private String bootstrapServers;
+
+    @Value("${spring.kafka.producer.ack}")
+    private String acknowledgement;
+
+    @Value("${spring.kafka.producer.number-of-tries}")
+    private Integer numberOfTries;
+
+    @Value("${spring.kafka.producer.imdepotence}")
+    private boolean imdepotence;
+
+    @Value("${spring.kafka.producer.timeout}")
+    private Integer timeOut;
+
+    @Value("${spring.kafka.schema-registry.url}")
+    private String schemaRegistry;
+
+
+    @Bean
+    public Map<String, Object> producerConfigs() {
+        Map<String, Object> props = new HashMap<>();
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.ACKS_CONFIG, acknowledgement);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.RETRIES_CONFIG, numberOfTries);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.REQUEST_TIMEOUT_MS_CONFIG, timeOut);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, imdepotence);
+
+        //Utilizando AVRO
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        props.put(org.apache.kafka.clients.producer.ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaProtobufSerializer.class);
+        props.put(AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG, schemaRegistry);
+        props.put(KafkaProtobufSerializerConfig.AUTO_REGISTER_SCHEMAS, true);
+        return props;
+    }
+
+    @Bean
+    public ProducerFactory<String, UsuarioTesteAvro> producerFactory() {
+        return new DefaultKafkaProducerFactory<>(producerConfigs());
+    }
+
+    @Bean
+    public KafkaTemplate<String, UsuarioTesteAvro> kafkaTemplate() {
+        return new KafkaTemplate<>(producerFactory());
+    }
+
+
+}
