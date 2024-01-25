ff817adb95c8bd6e62f965be2154d766d170f75a
Lucas Santos <lucas.parkl@outlook.com>
2024-01-24 22:50:27 -0300

    Spring boot 3 - Spring Kafka: - adicionando tratamento de erro ao postar mensagem

--- kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/gateways/KafkaPostUtils.java
+++ kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/gateways/KafkaPostUtils.java
@@ -4,7 +4,10 @@ package com.lucassilvs.kafkaproducerexemplo.gateways;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
 
 
 @Component
@@ -20,8 +23,16 @@ public class KafkaPostUtils {
 
     public void postarMensagem(Object mensagem, String nomeTopico){
         logger.info("Postando mensagem: {} no tópico {}", mensagem, nomeTopico);
-        kafkaTemplate.send(nomeTopico, mensagem);
+        CompletableFuture<SendResult<String, Object>> sendStatus = kafkaTemplate.send(nomeTopico, mensagem);
 
-        logger.info("mensagem enviada com sucesso");
-    }
+        sendStatus.whenComplete((result, exception) -> {
+            if (exception != null) {
+                logger.error("Erro ao enviar mensagem: {}", exception.getMessage());
+            } else {
+                logger.info("Mensagem enviada com sucesso:\nTopico: {} - particao: {} - offset: {}", result.getRecordMetadata().topic(),
+                        result.getRecordMetadata().partition(),
+                        result.getRecordMetadata().offset());
+            }
+        });
+        }
 }
