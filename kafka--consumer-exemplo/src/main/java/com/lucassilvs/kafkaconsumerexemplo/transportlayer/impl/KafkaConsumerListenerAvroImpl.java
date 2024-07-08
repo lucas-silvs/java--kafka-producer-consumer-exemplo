471f22da41a1e4fb7cd563f5d492969a559dfce3
Lucas Santos <lucas.parkl@outlook.com>
2024-07-08 01:15:19 -0300

    WIP: atualizando implementação producer/consumer: - criando profile local consumer - ajustando interface listener

--- kafka--consumer-exemplo/src/main/java/com/lucassilvs/kafkaconsumerexemplo/transportlayer/impl/KafkaConsumerListenerAvroImpl.java
+++ kafka--consumer-exemplo/src/main/java/com/lucassilvs/kafkaconsumerexemplo/transportlayer/impl/KafkaConsumerListenerAvroImpl.java
@@ -1,5 +1,6 @@
-package com.lucassilvs.kafkaconsumerexemplo.controller.impl;
+package com.lucassilvs.kafkaconsumerexemplo.transportlayer.impl;
 
+import com.lucassilvs.kafkaconsumerexemplo.transportlayer.KafkaConsumerListener;
 import com.lucassilvs.kafkaproducerexemplo.gateways.kafka.UsuarioTesteAvro;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.slf4j.Logger;
@@ -8,14 +9,14 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
-@Profile({"avro", "confluent", "oidc"})
+@Profile("avro")
 @Service
-public class KafkaConsumerListenerAvroImpl  {
+public class KafkaConsumerListenerAvroImpl implements KafkaConsumerListener<ConsumerRecord<String,UsuarioTesteAvro>> {
 
     private Logger logger = LoggerFactory.getLogger(KafkaConsumerListenerAvroImpl.class);
 
     @KafkaListener(topics = "${spring.kafka.nome-topico}", containerFactory = "kafkalisternerContainerFactory", groupId = "${spring.kafka.consumer.group-id}")
-    public void consumindoMensagemAvro(ConsumerRecord<String,UsuarioTesteAvro> mensagemSerialized) {
+    public void consumirMensagem(ConsumerRecord<String,UsuarioTesteAvro> mensagemSerialized) {
         UsuarioTesteAvro mensagem = mensagemSerialized.value();
 
         if(System.getenv("ENABLE_MOCK_LAG") != null && System.getenv("ENABLE_MOCK_LAG").equals("true")){
