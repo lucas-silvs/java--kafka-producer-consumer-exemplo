b04f821a9b27d0a62a43eb00e4831ea0678e9211
Lucas Santos <lucas.parkl@outlook.com>
2024-08-08 22:51:57 -0300

    ajustando validação se possui contrato avro ou protobuf para ativar o plugin

--- kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/interactors/impl/MensagemUseCaseImpl.java
+++ kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/interactors/impl/MensagemUseCaseImpl.java
@@ -2,24 +2,23 @@ package com.lucassilvs.kafkaproducerexemplo.interactors.impl;
 
 import com.lucassilvs.kafkaproducerexemplo.datasources.KafkaDataSourceImpl;
 import com.lucassilvs.kafkaproducerexemplo.datasources.kafka.UsuarioTesteAvro;
+import com.lucassilvs.kafkaproducerexemplo.repositories.EventRepository;
 import com.lucassilvs.kafkaproducerexemplo.transportlayer.models.MensagemRequest;
 import com.lucassilvs.kafkaproducerexemplo.interactors.MensagemUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
-@Profile({"avro", "confluent", "oidc"})
 @Service
 public class MensagemUseCaseImpl implements MensagemUseCase {
 
-    private final KafkaDataSourceImpl kafkaDataSourceImpl;
+    private final EventRepository eventRepository;
 
     private final String nomeTopico;
 
     @Autowired
     public MensagemUseCaseImpl(KafkaDataSourceImpl kafkaDataSourceImpl, @Value("${spring.kafka.nome-topico}") String nomeTopico) {
-        this.kafkaDataSourceImpl = kafkaDataSourceImpl;
+        this.eventRepository = kafkaDataSourceImpl;
         this.nomeTopico = nomeTopico;
     }
 
@@ -32,7 +31,7 @@ public class MensagemUseCaseImpl implements MensagemUseCase {
                     .setNome(mensagem.getMensagem())
                     .setSaldo(mensagem.getSaldo())
                     .build();
-            kafkaDataSourceImpl.sendEvent(usuarioTesteAvro, nomeTopico);
+            eventRepository.sendEvent(usuarioTesteAvro, nomeTopico);
         }
     }
 
