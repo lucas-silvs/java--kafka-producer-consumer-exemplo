e8352c1c3a5eca2d6f0c440ecb5c6d84435ef1e3
Lucas Santos <lucas.parkl@outlook.com>
2026-04-17 23:49:20 -0300

    separando camada de usecase por profile

--- kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/interactors/impl/MensagemUseCaseStringImpl.java
+++ kafka--producer-exemplo/src/main/java/com/lucassilvs/kafkaproducerexemplo/interactors/impl/MensagemUseCaseStringImpl.java
@@ -2,22 +2,24 @@ package com.lucassilvs.kafkaproducerexemplo.interactors.impl;
 
 import com.lucassilvs.kafkaproducerexemplo.datasources.KafkaDataSourceImpl;
 import com.lucassilvs.kafkaproducerexemplo.datasources.kafka.UsuarioTesteAvro;
+import com.lucassilvs.kafkaproducerexemplo.interactors.MensagemUseCase;
 import com.lucassilvs.kafkaproducerexemplo.repositories.EventRepository;
 import com.lucassilvs.kafkaproducerexemplo.transportlayer.models.MensagemRequest;
-import com.lucassilvs.kafkaproducerexemplo.interactors.MensagemUseCase;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MensagemUseCaseImpl implements MensagemUseCase {
+@Profile("string")
+public class MensagemUseCaseStringImpl implements MensagemUseCase {
 
     private final EventRepository eventRepository;
 
     private final String nomeTopico;
 
     @Autowired
-    public MensagemUseCaseImpl(KafkaDataSourceImpl kafkaDataSourceImpl, @Value("${spring.kafka.nome-topico}") String nomeTopico) {
+    public MensagemUseCaseStringImpl(KafkaDataSourceImpl kafkaDataSourceImpl, @Value("${spring.kafka.nome-topico}") String nomeTopico) {
         this.eventRepository = kafkaDataSourceImpl;
         this.nomeTopico = nomeTopico;
     }
@@ -26,12 +28,8 @@ public class MensagemUseCaseImpl implements MensagemUseCase {
 
 
         for (int i = 0; i < 10; i++) {
-            UsuarioTesteAvro usuarioTesteAvro = UsuarioTesteAvro.newBuilder()
-                    .setId(i)
-                    .setNome(mensagem.getMensagem())
-                    .setSaldo(mensagem.getSaldo())
-                    .build();
-            eventRepository.sendEvent(usuarioTesteAvro, nomeTopico);
+
+            eventRepository.sendEvent(mensagem.getMensagem(), nomeTopico);
         }
     }
 
